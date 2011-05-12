f3de3155c39479d1115cf7bba370d758f87af223
Remi Melisson <rmelisson@gmail.com>
2011-05-12 15:52:19 +0200

    javascript barely ok

--- nodejs/mizot/mizot.js
+++ nodejs/mizot/mizot.js
@@ -1,2 +1,15 @@
 var express = require("express")
 
+var app = express.createServer() 
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
+
+var pub = __dirname + '/public'
+app.use(express.static(pub));
+app.use(express.bodyParser());
+
+app.get('/', function(req, res) {
+	res.render('layout');
+});
+
+app.listen(8000);
--- nodejs/mizot/public/javascript/mizot.js
+++ nodejs/mizot/public/javascript/mizot.js
@@ -1,4 +1,31 @@
+switch_content = function(link) {
+	// we make the actual one disapear
+	id = $('.selected_link').attr('id');
+	$('.selected_link').removeClass('selected_link').addClass('link');
+	
+	content_id = id.substr(id.lastIndexOf('_') +1);
+	$('#' + content_id).addClass('invisible_content');
+	
+	//addClass('invisible_content');	
+
+	// then we display the right one
+	link.addClass('selected_link');
+	id = link.attr('id');
+	content_id = id.substr(id.lastIndexOf('_')+1);
+	$('#' + content_id).removeClass('invisible_content');
+	
+}
+
 $(document).ready(function(){
 	$('#header').corner("tr tl 10px");		
 	$('#home').corner("bl br 10px");		
+	$('#links').corner("5px");		
+	$('.link').corner("tl tr 12px");
+	$('.selected_link').corner("tl tr 12px");
+	$('.selected_link').click( function() {
+		switch_content($(this));		
+	});
+	$('.link').click( function() {
+		switch_content($(this));		
+	});
 });
