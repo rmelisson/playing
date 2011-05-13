4834eaf80cbc4d50cfa34649983ad8413af5205f
Remi Melisson <rmelisson@gmail.com>
2011-05-13 20:44:11 +0200

    improve it

--- nodejs/upload_form/up.js
+++ nodejs/upload_form/up.js
@@ -7,6 +7,18 @@ serv = http.createServer(function(req, res) {
   if (req.url == '/upload' && req.method.toLowerCase() == 'post') {
     // parse a file upload
     var form = new formidable.IncomingForm();
+		form.uploadDir = '.';
+		form.keepExtensions = true;
+form.on('progress', function(bytesReceived, bytesExpected) {
+  var progress = {
+    type: 'progress',
+    bytesReceived: bytesReceived,
+    bytesExpected: bytesExpected
+  };
+
+  console.log(JSON.stringify(progress));
+});
+
     form.parse(req, function(err, fields, files) {
       res.writeHead(200, {'content-type': 'text/plain'});
       res.write('received upload:\n\n');
@@ -19,7 +31,7 @@ serv = http.createServer(function(req, res) {
   res.writeHead(200, {'content-type': 'text/html'});
   res.end(
     '<form action="/upload" enctype="multipart/form-data" method="post">'+
-    '<input type="text" name="title"><br>'+
+//    '<input type="text" name="title"><br>'+
     '<input type="file" name="upload" multiple="multiple"><br>'+
     '<input type="submit" value="Upload">'+
     '</form>'
@@ -27,4 +39,4 @@ serv = http.createServer(function(req, res) {
 });
 
 
-serv.listen(8000);
+serv.listen(80);
