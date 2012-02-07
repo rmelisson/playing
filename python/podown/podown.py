181c4186f6e7ff62d72d2820bb5c4f611fa23d64
Remi <rmelisson@gmail.com>
2012-02-07 18:00:03 +0100

    barely work

--- python/podown/podown.py
+++ python/podown/podown.py
@@ -2,19 +2,23 @@ import feedparser
 from BeautifulSoup import BeautifulSoup
 
 #cww_feed_url = "http://chanceswithwolves.blogspot.com/feeds/posts/default"
-#cww_feed_url = "file:///home/saul/dev/playing/python/podown/100.xml"
-cww_feed_url = "file:///home/saul/dev/playing/python/podown/normal.xml"
+cww_feed_url = "file:///home/saul/dev/playing/python/podown/100.xml"
+#cww_feed_url = "file:///home/saul/dev/playing/python/podown/normal.xml"
+
+provider = "http://www.eastvillageradio.com"
 
 feed = feedparser.parse( cww_feed_url )
 
 print( feed[ "channel" ][ "title" ] )
 
 for item in feed[ "items" ]:
-    print( item[ "title" ] )
     html_content = item[ "content" ][0][ "value" ]
     #print html_content
     soup = BeautifulSoup( html_content )
     for a in soup.findAll( "a" ):
 	    #if a['href'].contains
-	    print a
-    print
+	    href = a.get( "href" )
+	    if (href.startswith(provider) and "PlayListID" in href) :
+		print( item[ "title" ] )
+	    	print( href )
+		print
