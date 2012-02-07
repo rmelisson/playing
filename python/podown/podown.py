27d390719b6ae67607738ae772d63cbdefcf69cc
Remi <rmelisson@gmail.com>
2012-02-07 17:03:43 +0100

    first steps

--- python/podown/podown.py
+++ python/podown/podown.py
@@ -1,7 +1,9 @@
 import feedparser
+from BeautifulSoup import BeautifulSoup
 
 #cww_feed_url = "http://chanceswithwolves.blogspot.com/feeds/posts/default"
-cww_feed_url = "file://home/saul/Downloads/cww.xml"
+#cww_feed_url = "file:///home/saul/dev/playing/python/podown/100.xml"
+cww_feed_url = "file:///home/saul/dev/playing/python/podown/normal.xml"
 
 feed = feedparser.parse( cww_feed_url )
 
@@ -9,3 +11,10 @@ print( feed[ "channel" ][ "title" ] )
 
 for item in feed[ "items" ]:
     print( item[ "title" ] )
+    html_content = item[ "content" ][0][ "value" ]
+    #print html_content
+    soup = BeautifulSoup( html_content )
+    for a in soup.findAll( "a" ):
+	    #if a['href'].contains
+	    print a
+    print
