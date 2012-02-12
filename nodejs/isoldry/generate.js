9fe4e1f38aa11939d5b895743fca60324f073320
saul <saul@home.(none)>
2012-02-12 16:15:34 +0100

    imic generate feature from mizot

---
+++ nodejs/isoldry/generate.js
@@ -0,0 +1,26 @@
+var fs = require('fs');
+var jade = require('jade');
+var util = require('util');
+var sys = require('sys')
+var exec = require('child_process').exec;
+
+var root_dir = '/tmp/isoldry';
+
+function puts(error, stdout, stderr) { sys.puts(stdout) }
+
+jade.renderFile('./views/layout.jade', function(err, html) {
+	if (err) {
+		console.log(err);
+	} else {
+		// we create the directory and flush html code into an index.html
+		fs.mkdir(root_dir, 0777 );
+		fs.writeFile(root_dir + "/index.html", html, function(err) {
+			if (err) {
+				console.log(err);
+			}
+		}); 
+		// we copy all the public files to the root directory
+		exec("cp -R ./public/* " + root_dir, puts);
+		console.log('... done');
+	}
+});
