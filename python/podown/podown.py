328c5d30434e0f451a8d1c073ccc741b2117a6cb
Remi <rmelisson@gmail.com>
2012-02-14 17:50:35 +0100

    wip podown

--- python/podown/podown.py
+++ python/podown/podown.py
@@ -1,17 +1,43 @@
 import feedparser
 import urllib2
+import threading
 from BeautifulSoup import BeautifulSoup
+
+#cww_feed_url = "http://chanceswithwolves.blogspot.com/feeds/posts/default"
+cww_feed_url = "file:///home/saul/dev/playing/python/podown/100.xml"
+#cww_feed_url = "file:///home/saul/dev/playing/python/podown/normal.xml"
+
+provider = "http://www.eastvillageradio.com"
+
+class Episode_Downloader(threading.Thread):
+    def __init__(self, url_base):
+        return
+
+    def download_episode(url_base, file_path):
+        ep_url = get_mp3_url( url_base )
+        url = urllib2.urlopen( ep_url )
+        f = open(file_path, 'w')
+        file_size_dl = 0
+        block_sz = 8192
+        while True:
+            buffer = url.read(block_sz)
+            if not buffer:
+                break
+            file_size_dl += len(buffer)
+            f.write(buffer)
+        f.close()
 
 def get_mp3_url(url_base):
     u = urllib2.urlopen(url_base)
-    open('/tmp/file', 'w')
     return u.read()
 
-#cww_feed_url = "http://chanceswithwolves.blogspot.com/feeds/posts/default"
-#cww_feed_url = "file:///home/saul/dev/playing/python/podown/100.xml"
-cww_feed_url = "file:///home/saul/dev/playing/python/podown/normal.xml"
+def get_filename_from_item(item):
+    if "Episode" in item["title"] :
+        return "yes!"
+    return "no"
 
-provider = "http://www.eastvillageradio.com"
+def create_ep_list(feed):
+    return
 
 feed = feedparser.parse( cww_feed_url )
 
@@ -22,11 +48,10 @@ for item in feed[ "items" ]:
     #print html_content
     soup = BeautifulSoup( html_content )
     for a in soup.findAll( "a" ):
-        #if a['href'].contains
         href = a.get( "href" )
         if (href.startswith(provider) and "PlayListID" in href) :
             print( item[ "title" ] )
+            print( get_filename_from_item(item) )
             print( href )
-            print get_mp3_url( href )
+            #download_episode( href )
             print
-    break
