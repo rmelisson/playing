285fc4ceb94dcdde00442ceb2fd0c64e1c3a4214
Remi <rmelisson@gmail.com>
2012-03-07 23:33:50 +0100

    podown alsmost done*

--- python/podown/podown.py
+++ python/podown/podown.py
@@ -10,13 +10,13 @@ cww_feed_url = "file:///home/saul/dev/playing/python/podown/100.xml"
 
 provider = "http://www.eastvillageradio.com"
 
-tmp_folder = "/tmp/podown/"
-if not os.path.exists(tmp_folder):
-        os.makedirs(tmp_folder)
+tmp_directory = "/tmp/podown/"
+if not os.path.exists(tmp_directory):
+    os.makedirs(tmp_directory)
 
-#class Episode_Downloader(threading.Thread):
-#    def __init__(self, url_base):
-#        return
+final_directory = "./down/"
+if not os.path.exists(final_directory):
+    os.makedirs(final_directory)
 
 def download_episode(url_base, file_path):
     ep_url = get_mp3_url( url_base )
@@ -31,6 +31,7 @@ def download_episode(url_base, file_path):
         file_size_dl += len(buffer)
         f.write(buffer)
     f.close()
+    return
 
 def get_mp3_url(url_base):
     u = urllib2.urlopen(url_base)
@@ -40,30 +41,49 @@ def get_filename_from_item(item):
     title = item["title"]
     if "EPISODE" in title.upper() :
         for word in title.split():
-            if word.startswith('#'):
+            if word.startswith('#') and len(word) > 1:
                 return word.strip('#') + '.mp3'
             elif word.isdigit():
                 return word + '.mp3'
     return '_'.join(title.strip('!)(').split()) + '.mp3'
 
+def get_title_list(html_content):
+    l = ""
+    titles = html_content.split('<br />')
+    # see problem with ep 182
+    if len(titles) < 10 :
+        titles = html_content.split('<div><span>')
+
+    for i in titles:
+        if not i.startswith('<') and i != "" :
+            t,_,div = i.partition('div')
+            l += t
+            l += '\n'
+    return l
+
+def is_already_there(item):
+    file_name = get_filename_from_item(item)
+    return file_name in os.listdir(final_directory)
+
 def create_ep_list(feed):
-    return
+    ep_list = []
+    #print( feed[ "channel" ][ "title" ] )
+    for item in feed[ "items" ]:
+        if not is_already_there(item):
+            html_content = item[ "content" ][0][ "value" ]
+            soup = BeautifulSoup( html_content )
+            for a in soup.findAll( "a" ):
+                href = a.get( "href" )
+                if (href.startswith(provider) and "PlayListID" in href) :
+                    ep_list.append( ( 
+                             get_filename_from_item(item),
+                             get_mp3_url(href),
+                             get_title_list(html_content) ) )
+    return ep_list
 
 feed = feedparser.parse( cww_feed_url )
-
-print( feed[ "channel" ][ "title" ] )
-
-for item in feed[ "items" ]:
-    html_content = item[ "content" ][0][ "value" ]
-    #print html_content
-    soup = BeautifulSoup( html_content )
-    for a in soup.findAll( "a" ):
-        href = a.get( "href" )
-        if (href.startswith(provider) and "PlayListID" in href) :
-            #print( item[ "title" ] )
-            print( get_filename_from_item(item) )
-            #print( href )
-            #download_episode( href )
-            #print get_mp3_url( href )
-            print html_content.split('<br />')
-    break
+for (filename, mp3_url, titles) in create_ep_list(feed):
+    print filename
+    print mp3_url
+    print titles
+    print
