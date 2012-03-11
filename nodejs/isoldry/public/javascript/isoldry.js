fa2e53a4684967342c92645f4a582201d43416dd
Remi <rmelisson@gmail.com>
2012-03-11 16:06:36 +0100

    main design done

--- nodejs/isoldry/public/javascript/isoldry.js
+++ nodejs/isoldry/public/javascript/isoldry.js
@@ -1,3 +1,26 @@
+switch_content = function(link) {
+	// we make the actual one disapear
+	id = $('.selected_link').attr('id');
+	$('.selected_link').removeClass('selected_link').addClass('link');
+	
+	content_id = id.substr(id.lastIndexOf('_') +1);
+	$('#' + content_id).addClass('invisible_content');
+	
+	// then we display the right one
+	link.addClass('selected_link');
+	id = link.attr('id');
+	content_id = id.substr(id.lastIndexOf('_')+1);
+	$('#' + content_id).removeClass('invisible_content');
+	
+}
+
 $(document).ready(function() {
     $('#main').corner("tl tr bl br 20px");
+
+	$('.selected_link').click( function() {
+		switch_content($(this));		
+	});
+	$('.link').click( function() {
+		switch_content($(this));		
+	});
 });
